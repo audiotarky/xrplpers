7958a281bfce659335096a879cb3d1da19fc8784
Simon Metson <drsm79@Simons-MacBook-Air.local>
2021-11-25 16:22:46 +0000

    nftests

---
+++ test/test_nft_TransferFee.py
@@ -0,0 +1,33 @@
+import unittest
+from xrplpers.nfts.entities import TransferFee
+
+
+class testVerification(unittest.TestCase):
+    def testLowerBound(self):
+        with self.assertRaises(ValueError):
+            TransferFee(-1)
+
+    def testUpperBound(self):
+        with self.assertRaises(ValueError):
+            TransferFee(5001)
+
+    def testLowerBoundFromPercent(self):
+        with self.assertRaises(ValueError):
+            TransferFee.from_percent(-1)
+
+    def testUpperBoundFromPercent(self):
+        with self.assertRaises(ValueError):
+            TransferFee.from_percent(51)
+
+    def testFromPercent(self):
+        t = TransferFee.from_percent(50)
+        self.assertEqual(t.value, t.max)
+        self.assertEqual(t.as_percent(), 50)
+
+    def testMustBeInt(self):
+        with self.assertRaises(TypeError):
+            TransferFee("foo")
+
+    def testMustBeIntFromPercent(self):
+        with self.assertRaises(TypeError):
+            TransferFee.from_percent("foo")
